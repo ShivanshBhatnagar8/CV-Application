c8f246cad358b50fa6d8f9547aee60c6498ce3f1
ShivanshBhatnagar8 <shivanshbhatnagar8@gmail.com>
2024-04-21 01:20:11 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import "./styles/style.css";
+//import './App.css'
+
+function App() {
+  return (
+    <>
+      <header>
+        <h1>CV Application</h1>
+        <a href="#" className="btn-download">
+          Click to Download
+        </a>
+      </header>
+      <div className="main-container"></div>
+    </>
+  );
+}
+
+export default App;

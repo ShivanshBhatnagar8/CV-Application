92f881f125e3a4899e859e4bb46b2ae6bb3c8c65
ShivanshBhatnagar8 <shivanshbhatnagar8@gmail.com>
2024-06-02 18:12:19 +0530

    Created object for Input fields of Education Section

---
+++ src/utils/EducationSectionFields.jsx
@@ -0,0 +1,34 @@
+const educationFields = [
+  {
+    Id: 1,
+    name: "Institution",
+    type: "text",
+    value: "",
+  },
+  {
+    Id: 2,
+    name: "Course",
+    type: "text",
+    value: "",
+  },
+  {
+    Id: 3,
+    name: "Title",
+    type: "text",
+    value: "",
+  },
+  {
+    Id: 4,
+    name: "Start Date",
+    type: "date",
+    value: "",
+  },
+  {
+    Id: 5,
+    name: "End Date",
+    type: "date",
+    value: "",
+  },
+];
+
+export default educationFields;

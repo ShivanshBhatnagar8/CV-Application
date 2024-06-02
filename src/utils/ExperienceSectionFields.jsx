924e7072bd8e3f68d34c87192606e493ca697f3c
ShivanshBhatnagar8 <shivanshbhatnagar8@gmail.com>
2024-06-02 18:12:51 +0530

    Created object for Input fields of Experience  Section

---
+++ src/utils/ExperienceSectionFields.jsx
@@ -0,0 +1,9 @@
+const experienceFields = [
+  { Id: 1, name: "Company", type: "text", value: "" },
+  { Id: 2, name: "Position", type: "text", value: "" },
+  { Id: 3, name: "Description", type: "text", value: "" },
+  { Id: 4, name: "Start Date", type: "date", value: "" },
+  { Id: 5, name: "End Date", type: "date", value: "" },
+];
+
+export default experienceFields;

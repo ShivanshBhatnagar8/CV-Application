bc19a6675fe694834bb49514e79a0956b90b44ba
ShivanshBhatnagar8 <shivanshbhatnagar8@gmail.com>
2024-06-02 18:14:11 +0530

    Created object for Input fields of General Information Section

---
+++ src/utils/GeneralSectionFields.jsx
@@ -0,0 +1,12 @@
+const generalFields = [
+  { Id: 1, name: "First Name", type: "text", value: "" },
+  { Id: 2, name: "Last Name", type: "text", value: "" },
+  { Id: 3, name: "Title", type: "text", value: "" },
+  { Id: 4, name: "Email", type: "email", value: "" },
+  { Id: 5, name: "Phone", type: "tel", value: "" },
+  { Id: 6, name: "Description", type: "text", value: "" },
+  { Id: 7, name: "Github Link", type: "url", value: "" },
+  { Id: 8, name: "Linkedin Link", type: "url", value: "" },
+];
+
+export default generalFields;

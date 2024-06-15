d51f3e7927efd0a7fd58d993a9c1fa07df77b124
Shivansh Bhatnagar <shivanshbhatnagar8@gmail.com>
2024-06-15 15:31:25 +0530

    Removed description  element from the array

--- src/utils/GeneralSectionFields.jsx
+++ src/utils/GeneralSectionFields.jsx
@@ -4,9 +4,8 @@ const generalFields = [
   { Id: 3, name: "Title", type: "text", value: "" },
   { Id: 4, name: "Email", type: "email", value: "" },
   { Id: 5, name: "Phone", type: "tel", value: "" },
-  { Id: 6, name: "Description", type: "text", value: "" },
-  { Id: 7, name: "Github Link", type: "url", value: "" },
-  { Id: 8, name: "Linkedin Link", type: "url", value: "" },
+  { Id: 6, name: "Github Link", type: "url", value: "" },
+  { Id: 7, name: "Linkedin Link", type: "url", value: "" },
 ];
 
 export default generalFields;
